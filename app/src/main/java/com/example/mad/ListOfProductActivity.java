6c79da7cb727ad00ad566fa32871fee1a470eaef
arunalk722 <arunalk722@hotmail.com>
2024-02-24 12:05:04 +0530

    24-2-2024 00.31

--- app/src/main/java/com/example/mad/ListOfProductActivity.java
+++ app/src/main/java/com/example/mad/ListOfProductActivity.java
@@ -1,14 +1,49 @@
 package com.example.mad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.ArrayList;
 
 public class ListOfProductActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_product);
+
+        listView = findViewById(R.id.lstProducts);
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        db = FirebaseFirestore.getInstance();
+        retrieveProducts();
+    }
+
+    private void retrieveProducts() {
+        db.collection("Product_List")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String productName = document.getString("proId");
+                            items.add(productName);
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(this, "Failed to retrieve products", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/mad/ProductMngActivity.java
+++ app/src/main/java/com/example/mad/ProductMngActivity.java
@@ -15,22 +15,31 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProductMngActivity extends AppCompatActivity {
-    private  FirebaseAuthClass firebaseAuthClass = new FirebaseAuthClass();
-    private  EditText proIdTxt, proNameTxt, proPriceTxt, proQtyTxt, proDisTxt;
-    private  Button findBtn, newProductBtn,clearBtn,updateBtn,imageSelectBtn,imageUploadBtn;
-    private  ProgressBar savePBar, findPBar;
-    private    boolean isNewProduct = true;
+    private FirebaseAuthClass firebaseAuthClass = new FirebaseAuthClass();
+    private EditText proIdTxt, proNameTxt, proPriceTxt, proQtyTxt, proDisTxt;
+    private Button findBtn, newProductBtn, clearBtn, updateBtn, imageSelectBtn, imageUploadBtn;
+    private ProgressBar savePBar, findPBar,imageUploadPBar;
+    private boolean isNewProduct = true;
     private Uri filePath;
     private final int PICK_IMAGE_REQUEST = 22;
-    private   ImageView imageView;
-
+    private ImageView imageView;
+    private StorageReference storageRef;
+    Uri defaultProductImage = Uri.parse("https://firebasestorage.googleapis.com/v0/b/mobile-app-dev-icbt.appspot.com/o/images%2Fcake-candles.svg?alt=media&token=8e88f899-80ed-459e-9df4-3b3b52946ed8");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +48,7 @@ public class ProductMngActivity extends AppCompatActivity {
 
         hideSavePrograss();
         hideFindPBar();
+       // hideUploadImage();
 
         newProductBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,13 +61,13 @@ public class ProductMngActivity extends AppCompatActivity {
         findBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               if(proIdTxt.length()>=1){
-                   showFindPBar();
-                   ProductInfo pi = new ProductInfo();
-                   Integer id = Integer.parseInt(proIdTxt.getText().toString());
-                   String productId = String.format("%08d", id);
-                   productFind(productId);
-               }
+                if (proIdTxt.length() >= 1) {
+                    showFindPBar();
+                    ProductInfo pi = new ProductInfo();
+                    Integer id = Integer.parseInt(proIdTxt.getText().toString());
+                    String productId = String.format("%08d", id);
+                    productFind(productId);
+                }
 
             }
         });
@@ -72,9 +82,9 @@ public class ProductMngActivity extends AppCompatActivity {
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(proIdTxt.length()==8){
+                if (proIdTxt.length() == 8) {
                     showSavePrograss();
-                    saveProductInfo(proIdTxt.getText().toString());
+                    uploadImage(filePath, proIdTxt.getText().toString());
                 }
             }
         });
@@ -82,13 +92,14 @@ public class ProductMngActivity extends AppCompatActivity {
         imageSelectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 SelectImage();
             }
+
         });
     }
-    private void SelectImage()
-    {
+
+
+    private void SelectImage() {
 
         // Defining Implicit Intent to mobile gallery
         Intent intent = new Intent();
@@ -103,22 +114,16 @@ public class ProductMngActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            // Get the Uri of the selected image
             filePath = data.getData();
             try {
-                // Load the selected image into the ImageView
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
                 imageView.setImageBitmap(bitmap);
             } catch (IOException e) {
-                // Log the exception
                 e.printStackTrace();
-                // Show an error message to the user
                 Toast.makeText(this, "Error loading image", Toast.LENGTH_SHORT).show();
             }
         } else {
-            // Show a message indicating that no image was selected
             Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
         }
     }
@@ -131,25 +136,16 @@ public class ProductMngActivity extends AppCompatActivity {
         proDisTxt = (EditText) findViewById(R.id.txtDiscount);
         findBtn = (Button) findViewById(R.id.btnFoundProduct);
         newProductBtn = (Button) findViewById(R.id.btnNewProduct);
-
         imageSelectBtn = (Button) findViewById(R.id.btnSelectImage);
-        imageUploadBtn = (Button) findViewById(R.id.btnUploadImage);
-
-
         updateBtn = (Button) findViewById(R.id.btnUpdate);
         clearBtn = (Button) findViewById(R.id.btnClear);
         savePBar = (ProgressBar) findViewById(R.id.prgBarSave);
         findPBar = (ProgressBar) findViewById(R.id.prgFind);
-
         imageView = findViewById(R.id.imgViewPrImg);
     }
 
     void productSave() {
 
-
-//       if(proIdTxt.getText().toString().length()<2){
-//
-//       }
         if (proNameTxt.getText().toString().length() < 2) {
             proNameTxt.setError("Please provide name for product");
         } else if (proPriceTxt.getText().toString() == null) {
@@ -166,10 +162,9 @@ public class ProductMngActivity extends AppCompatActivity {
                 firebaseAuthClass.getNextId("Product_List", new FirebaseAuthClass.NextIdCallback() {
                     @Override
                     public void onNextId(int nextId) {
-                        // SystemOprations.showMessage("A","error on product id create",ProductMngActivity.this,3);
+
                         String productId = String.format("%08d", nextId);
-                        saveProductInfo(productId);
-                        hideSavePrograss();
+                        uploadImage(filePath,productId);
                     }
 
                     @Override
@@ -181,10 +176,38 @@ public class ProductMngActivity extends AppCompatActivity {
             } else {
 
                 proIdTxt.setText((int) Double.parseDouble(proIdTxt.getText().toString()));
-                saveProductInfo(proIdTxt.getText().toString());
+                uploadImage(filePath, proIdTxt.getText().toString());
 
             }
         }
+
+    }
+
+    void uploadImage(Uri imageUri,String productId){
+        storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imageRef = storageRef.child("product/" + productId);
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Toast.makeText(getApplicationContext(), "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String downloadUrl = uri.toString();
+
+                                saveProductInfo(productId, Uri.parse(downloadUrl));
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Image upload failed: " + e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
@@ -197,6 +220,10 @@ public class ProductMngActivity extends AppCompatActivity {
                 proDisTxt.setText(documentSnapshot.getDouble("Discount").toString());
                 proQtyTxt.setText(documentSnapshot.getDouble("Quantity").toString());
                 proPriceTxt.setText(documentSnapshot.getDouble("Price").toString());
+                Glide.with(ProductMngActivity.this)
+                        .load(documentSnapshot.getString("imagePath"))
+                        .placeholder(null)
+                        .into(imageView);
                 clearError();
                 hideFindPBar();
             }
@@ -223,9 +250,10 @@ public class ProductMngActivity extends AppCompatActivity {
         proDisTxt.setText("");
         proQtyTxt.setText("");
         proPriceTxt.setText("");
+        imageView.setImageBitmap(null);
     }
 
-    void saveProductInfo(String productId) {
+    void saveProductInfo(String productId,Uri imageUrl) {
         Map<String, Object> productInfo = new HashMap<>();
         productInfo.put("proId", productId);
         productInfo.put("productName", proNameTxt.getText().toString());
@@ -233,6 +261,7 @@ public class ProductMngActivity extends AppCompatActivity {
         productInfo.put("Quantity", Double.parseDouble(proQtyTxt.getText().toString()));
         productInfo.put("Discount", Double.parseDouble(proDisTxt.getText().toString()));
         productInfo.put("DateTime", SystemOprations.curretDate().toString());
+        productInfo.put("imagePath", imageUrl);
         firebaseAuthClass.intFirebaseFireStore(productInfo, "Product_List", productId, new FirebaseAuthClass.FirestoreCallback() {
             @Override
             public void onSuccess() {
@@ -247,29 +276,7 @@ public class ProductMngActivity extends AppCompatActivity {
             }
         });
     }
-    void updateProductInfo(String productId) {
-        Map<String, Object> productInfo = new HashMap<>();
-        productInfo.put("proId", productId);
-        productInfo.put("productName", proNameTxt.getText().toString());
-        productInfo.put("Price", Double.parseDouble(proPriceTxt.getText().toString()));
-        productInfo.put("Quantity", Double.parseDouble(proQtyTxt.getText().toString()));
-        productInfo.put("Discount", Double.parseDouble(proDisTxt.getText().toString()));
-        productInfo.put("DateTime", SystemOprations.curretDate().toString());
-        firebaseAuthClass.intFirebaseFireStore(productInfo, "Product_List", productId, new FirebaseAuthClass.FirestoreCallback() {
-            @Override
-            public void onSuccess() {
-                SystemOprations.showMessage("New Product save successful.\nproduct Id " + productId, "New Product save", ProductMngActivity.this, 1);
-                hideSavePrograss();
-            }
-
-            @Override
-            public void onFailure(Exception error) {
-                SystemOprations.showMessage(error.getMessage(), "error on product saving", ProductMngActivity.this, 3);
-                hideSavePrograss();
-            }
-        });
-    }
-    void showSavePrograss() {
+   void showSavePrograss() {
         newProductBtn.setVisibility(View.INVISIBLE);
         clearBtn.setVisibility(View.INVISIBLE);
         updateBtn.setVisibility(View.INVISIBLE);
@@ -296,4 +303,5 @@ public class ProductMngActivity extends AppCompatActivity {
         findBtn.setVisibility(View.VISIBLE);
         findPBar.setVisibility(View.INVISIBLE);
     }
+
 }
